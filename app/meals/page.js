7c33be3d3c252237185c729946cbe2ea3a5480b8
Carter Larsen <cjl259@cornell.edu>
2025-07-05 23:42:27 -0400

    Add Meal Storage and Display
    Only dummy meals right now. Also, can't see meal details yet. Also added error page for meals and not found page. Also have loading indicator if meals take time to load from database.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,20 +1,34 @@
 import Link from "next/link";
+import cssClasses from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <ul>
-        <li>
-          <Link href="meals/detail-1">detail-1</Link>
-        </li>
-        <li>
-          <Link href="meals/detail-2">detail-2</Link>
-        </li>
-        <li>
-          <Link href="meals/share">Share!</Link>
-        </li>
-      </ul>
-    </main>
+    <>
+      <header className={cssClasses.header}>
+        <h1>
+          Delicious Meals for Created{" "}
+          <span className={cssClasses.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe to make and enjoy.</p>
+        <p className={cssClasses.participate}>
+          <Link href="meals/share">Share a Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <Suspense
+          fallback={<p className={cssClasses.loading}>Loading Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>This resource or page was not found.</p>
+    </main>
+  );
+}
